0b95407516827660118d3dc3e041253f6b9ac31f
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-18 13:48:29 +0200

    Primative variables

---
+++ basics/src/Primitives.java
@@ -0,0 +1,19 @@
+public class Primitives {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        int i = 1;
+        System.out.println(i);
+        int j;
+        j = 2;
+        System.out.println(j);
+        long d = 1300000000;
+        System.out.println(d);
+        double myDouble = 17;
+        System.out.println(myDouble);
+        var anotherVariable = 14;
+        System.out.println(anotherVariable);
+        int sum = j +i;
+        System.out.println(sum);
+    }
+}
