3b3d4495f6f7b39b226421f7ced643d34bee6436
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-18 15:34:33 +0200

    Loops, for loop

---
+++ basics/src/Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+
+    public static void main(String[] args) {
+        for (int i = 50; i <=55; i++){
+            System.out.println(i*3);
+        }
+
+        double[] array = {13.5,12.7, 1.6};
+        for(int i = 0; i < array.length; i++){
+            System.out.println(i);
+        }
+    }
+}
