92825335f27f3f6cbdf2347a42ccf72beb89cc54
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-18 15:48:33 +0200

    Conditions, if

---
+++ basics/src/Conditions.java
@@ -0,0 +1,14 @@
+public class Conditions {
+
+    public static void main(String[] args) {
+        int testScore = 40;
+
+        if (testScore >= 50){
+            System.out.println("Well done -  you passed");
+        }
+
+        if (testScore < 50){
+            System.out.println("Sorry -  you failed");
+        }
+    }
+}
