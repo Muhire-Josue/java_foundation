404bad2fc8ab8503524d15db3f76c92dfa93d732
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-22 10:45:34 +0200

    Classes, Employee

---
+++ basics/src/classes/Employee.java
@@ -0,0 +1,49 @@
+package classes;
+
+public class Employee {
+    private String title;
+    private String firstName;
+    private String surname;
+    private String jobTitle;
+    private String department;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
