4a17a496a6dd5dbf749fa83b70f1d8641ebceb4f
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-23 10:51:20 +0200

    Inheritance, TrainTicket extends TravelTicket class

---
+++ basics/src/oop/inheritance/Main.java
@@ -0,0 +1,8 @@
+package oop.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        TrainTicket trainTicket = new TrainTicket();
+        trainTicket.cancel();
+    }
+}
