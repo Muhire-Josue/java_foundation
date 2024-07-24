9bd71b22292564ed50ffeca1de14971c1b56ee07
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-24 11:50:16 +0200

    Exceptions, arrayOutOfBound

---
+++ basics/src/exceptions/Exceptions.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+        Integer[] integers = new Integer[] {1,2,3,4};
+        System.out.println(integers[9]);
+    }
+}
