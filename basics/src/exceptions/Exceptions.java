f38e77d113bd820c7f073e31eedbe6e265e1d82c
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-24 11:52:03 +0200

    Exceptions, arithmeticException

--- basics/src/exceptions/Exceptions.java
+++ basics/src/exceptions/Exceptions.java
@@ -2,8 +2,13 @@ package exceptions;
 
 public class Exceptions {
 
+    public static void divideByZero(){
+        System.out.println(17/0);
+    }
+
     public static void main(String[] args) {
         Integer[] integers = new Integer[] {1,2,3,4};
-        System.out.println(integers[9]);
+//        System.out.println(integers[9]);
+        divideByZero();
     }
 }
