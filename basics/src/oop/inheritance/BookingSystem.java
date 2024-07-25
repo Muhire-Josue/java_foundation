6967505f3b49fad99037460adb832d1e33d1acd4
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-25 12:58:57 +0200

    Inheritance, creates BookingSystem class

---
+++ basics/src/oop/inheritance/BookingSystem.java
@@ -0,0 +1,8 @@
+package oop.inheritance;
+
+public interface BookingSystem {
+
+    public void setTravelTicket(TravelTicket ticket);
+    public void requestBooking();
+    public boolean getStatus();
+}
