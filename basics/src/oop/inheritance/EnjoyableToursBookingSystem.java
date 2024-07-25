1fa9c0cf5f47de0b42b2b3b1131f7f726ef6888a
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-25 13:06:53 +0200

    Inheritance, EnjoyableToursBookingSystem implements BookingSystem interface

--- basics/src/oop/inheritance/EnjoyableToursBookingSystem.java
+++ basics/src/oop/inheritance/EnjoyableToursBookingSystem.java
@@ -3,16 +3,17 @@ package oop.inheritance;
 public class EnjoyableToursBookingSystem implements BookingSystem{
     @Override
     public void setTravelTicket(TravelTicket ticket) {
-
+        System.out.println("Enjoyable Tours has recived ticket" + ticket);
     }
 
     @Override
     public void requestBooking() {
-
+        System.out.println("Enjoyable Tours received a booking request");
     }
 
     @Override
     public boolean getStatus() {
+        System.out.println("Enjoyable Tours is providing the booking status");
         return false;
     }
 }
